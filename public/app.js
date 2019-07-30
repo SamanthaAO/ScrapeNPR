bd60f3988164d5fc1ef8c6c46140f9f019a7e2ee
SamanthaAO <samanthaaorcutt@gmail.com>
2019-07-29 22:55:36 -0700

    added modal for comments

--- public/app.js
+++ public/app.js
@@ -13,6 +13,11 @@ $(document).on("click", "#scrape", function () {
     })
 })
 
+$(document).on("click", "#leaveComment", function () {
+    $('#myModal').modal('toggle')
+})
+
+
 var getArticles = (category)=> {
     $("#articles").empty();
     $.getJSON("/articles/" + category, function (data) {
@@ -29,7 +34,7 @@ var getArticles = (category)=> {
                         <h5 class="card-title">${data[i].title}</h5>
                         <p class="card-text">${data[i].summary}</p>
                     <a href="${data[i].link}" class="btn btn-primary">View Article</a>
-                    <a href="#" class="btn btn-primary">Leave Note</a>
+                    <a href="#" id="leaveComment"class="btn btn-primary">Leave Comment</a>
                     </div>
                 </div>
             </div>`);
@@ -40,15 +45,4 @@ var getArticles = (category)=> {
 }
 
 getArticles("national");
-//   <div class="card-header">
-//                 <ul class="nav nav-tabs card-header-tabs">
-//                     <li class="nav-item">
-//                         <a class="nav-link active" href="#">Active</a>
-//                     </li>
 
-//                     <li class="nav-item">
-//                         <a class="nav-link" href="#">Link</a>
-//                     </li>
-
-//                 </ul>
-//                 </div>
