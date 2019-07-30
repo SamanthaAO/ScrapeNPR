c5970e8ff2510f9ee3c4029dedab7f66be537691
SamanthaAO <samanthaaorcutt@gmail.com>
2019-07-30 00:01:00 -0700

    added not recording capability to app

--- public/app.js
+++ public/app.js
@@ -14,19 +14,48 @@ $(document).on("click", "#scrape", function () {
 })
 
 $(document).on("click", "#leaveComment", function () {
+    // Empty the notes from the note section
+    $("#previousComments").empty();
+    $("#saveCommentDiv").empty();
+
     $('#myModal').modal('toggle')
+    
+    
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // With that done, add the note information to the page
+      .then(function(data) {
+        console.log(data);
+        // A button to submit a new note, with the id of the article saved to it
+
+        $("#saveCommentDiv").append(`<button id ="saveComment" data-id="${data._id}" type="button" class="btn btn-primary">Save Comment</button>`);
+        
+        // If there's a note in the article
+        if (data.note) {
+          // Place the title of the note in the title input
+          $("#previousComments").append(`<strong>${data.note.title}:</strong>`);
+          // Place the body of the note in the body textarea
+          $("#previousComments").append(`${data.note.body} <br>`);
+        }
+      });
 })
 
 
 var getArticles = (category)=> {
     $("#articles").empty();
-    $.getJSON("/articles/" + category, function (data) {
+    $.getJSON("/articles/category/" + category, function (data) {
         // For each one
         for (var i = 0; i < data.length; i++) {
             // Display the apropos information on the page
             $("#articles").append(
                 `<div class="col-sm-3">
-                <div data-id="${data[i]._id}"class="card" style="width: 18rem;">
+                <div data-id="${data[i]._id}" class="card" style="width: 18rem;">
                 
     
                     <img class="card-img-top" src="${data[i].image}" alt="${data[i].title}">
@@ -34,7 +63,7 @@ var getArticles = (category)=> {
                         <h5 class="card-title">${data[i].title}</h5>
                         <p class="card-text">${data[i].summary}</p>
                     <a href="${data[i].link}" class="btn btn-primary">View Article</a>
-                    <a href="#" id="leaveComment"class="btn btn-primary">Leave Comment</a>
+                    <a href="#" id="leaveComment" data-id="${data[i]._id}" class="btn btn-primary">Leave Comment</a>
                     </div>
                 </div>
             </div>`);
@@ -45,4 +74,37 @@ var getArticles = (category)=> {
 }
 
 getArticles("national");
+
+
+
+
+  // When you click the savenote button
+  $(document).on("click", "#saveComment", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#nameInput").val(),
+        // Value taken from note textarea
+        body: $("#commentInput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        //$("#previousComments").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#nameInput").val("");
+    $("#commentInput").val("");
+  });
+  
 
--- server.js
+++ server.js
@@ -143,73 +143,12 @@ app.get("/scrape", function (req, res) {
 
 });
 
-// var scrape = (i) => {
-//     return 
-//         var $ = cheerio.load(response.data);
-       
-//         if (i < categories.legth){
-//             $("article.item.has-image").each(function (i, element) {
-
-//                 // Then, we load that into cheerio and save it to $ for a shorthand selector
-//                 var $ = cheerio.load(response.data);
-    
-//                 // Save an empty result object
-//                 var result = {};
-    
-//                 // Add the text and href of every link, and save them as properties of the result object
-//                 result.category = categories[i].category;
-    
-//                 result.title = $(this)
-//                     .children("div.item-info-wrap")
-//                     .children("div.item-info")
-//                     .children("h2.title")
-//                     .children("a")
-//                     .text();
-//                 result.link = $(this)
-//                     .children("div.item-info-wrap")
-//                     .children("div.item-info")
-//                     .children("h2.title")
-//                     .children("a")
-//                     .attr("href");
-//                 result.summary = $(this)
-//                     .children("div.item-info-wrap")
-//                     .children("div.item-info")
-//                     .children("p.teaser")
-//                     .children("a")
-//                     .text();
-//                 result.image = $(this)
-//                     .children("div.item-image")
-//                     .children("div.imagewrap")
-//                     .children("a")
-//                     .children("img")
-//                     .attr("src");
-    
-//                 // Create a new Article using the `result` object built from scraping
-//                 db.Article.findOneAndUpdate({ link: result.link }, result, { upsert: true })
-//                     .then(function (dbArticle) {
-//                         // View the added result in the console
-//                         console.log(dbArticle);
-//                     })
-//                     .catch(function (err) {
-//                         // If an error occurred, log it
-//                         console.log(err);
-//                     });
-//             });
-//             scrape(i + 1);
-//         }
-//         else {
-//             // Send a message to the client
-//             //res.send("Scrape Complete");
-//             return "success";
-//         }
-//     });
-
 
 
 // }
 
 // Route for getting all Articles from the db
-app.get("/articles/:category", function (req, res) {
+app.get("/articles/category/:category", function (req, res) {
     // Grab every document in the Articles collection
     db.Article.find({ category: req.params.category })
         .then(function (dbArticle) {
@@ -222,41 +161,41 @@ app.get("/articles/:category", function (req, res) {
         });
 });
 
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Article.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-// // Route for saving/updating an Article's associated Note
-// app.post("/articles/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   db.Note.create(req.body)
-//     .then(function(dbNote) {
-//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(function(dbArticle) {
-//       // If we were able to successfully update an Article, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 
 app.get("/", function (req, res) {
