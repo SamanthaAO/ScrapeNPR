b1d5565b3cf7ed27f4f6fe4d12a9c8fbc86d377d
SamanthaAO <samanthaaorcutt@gmail.com>
2019-07-30 18:10:32 -0700

    added heroku material

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -38,7 +38,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18HW", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/unit18HW", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18HW";
+
+mongoose.connect(MONGODB_URI);
+
 
 var categories = [
     {
